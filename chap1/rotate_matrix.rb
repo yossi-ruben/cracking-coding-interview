baeb7f9c14e3b08224dbdaabf531e450ddb1424e
Yossi Ruben <yossimruben@gmail.com>
2016-09-22 17:38:54 -0400

    more to complete

---
+++ chap1/rotate_matrix.rb
@@ -0,0 +1,14 @@
+# 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+# Before
+# [1,2,3,
+#  4,5,6,
+#  7,8,9]
+# after
+#  [7,4,1,
+#   8,5,2
+#   9,6,3]
+
+def rotate_matrix(array)
+
+
+end
